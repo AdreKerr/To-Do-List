27381bcb6540e8fbb8ab2039f1794c95486d1e70
AdreKerr <166549482+AdreKerr@users.noreply.github.com>
2024-09-05 10:20:11 -0500

    update Style
    Updating the style to look how i wanted and writing some comments of what i want in the js page

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,39 @@
 // JS CODE
+
+// type in to add to list after clicking ADD should show up to the list
+// TITLE TO ADD ITEMS TO LIST
+let AddItem = document.getElementById("AddToList");
+let AddBtn = document.getElementById("Add");
+AddBtn.addEventListener("click",()=>{
+    //first add item to display list
+    // DISPLAYS
+    DisplayTask();
+    //then clear from input so you can add a new item to list
+    // CLEARS
+    AddItem.value = "";
+})
+
+
+
+// dispaly funtion ????
+//DISPLAY ????????
+let displayList = [
+    {
+        checkMark :"",
+        // == AddItem.value = ItemOnList
+        ItemOnList: "",
+        Xmark: ""
+    }
+]
+
+
+//funtions
+// Display tasks
+function DisplayTask(){
+    //object displayList
+    //figure out how to get it to work
+    //.push to keep adding 
+
+}//end function display tast
+
+

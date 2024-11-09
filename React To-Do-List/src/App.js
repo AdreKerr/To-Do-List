afd3b83d62ce66a22e19824c4647b94870f48406
AdreKerr <adreauna.kerr@mscoding.org>
2024-11-09 10:08:54 -0600

    react

--- React To-Do-List/src/App.js
+++ React To-Do-List/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 // pages
-
+import Header from "./components/Header";
+import Title from "./components/Title";
+import ListDisplay from "./components/ListDisplay";
 // style
+import './style.css';
 
 function App() {
-    <div className='container'>
+    <div className='big'>
 
         {/* pages go here */}
+        <Header />
+        <Title />
+        <ListDisplay />
 
     </div>
 }
